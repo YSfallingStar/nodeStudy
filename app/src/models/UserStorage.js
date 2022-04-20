be38727b5a2a85d3ee909dcea121907259fa224e
YSfallingStar <wjddbtjd55@naver.com>
2022-04-20 10:05:14 +0900

    login 문법 간략화

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -6,7 +6,7 @@ class UserStorage{
         pw: ["1234", "1234", "1234"],
         name: ["유성", "정개발", "정팀장"],
     };
-
+ 
     static getUsers(...fields) {
         const users = this.#users;
         const newUsers = fields.reduce((newUsers, field) => {
@@ -17,6 +17,16 @@ class UserStorage{
             return newUsers;
         }, {});
         return newUsers;
+    }
+    static getUserInfo(id) {
+        const users = this.#users;
+        const idx = users.id.indexOf(id);
+        const usersKeys = Object.keys(users);   // [id, pw, name] 여러 배열로 가져옴
+        const userInfo = usersKeys.reduce((newUser, info) => {
+            newUser[info] = users[info][idx];
+            return newUser;
+        }, {});
+        return userInfo;
     }
 }
 
